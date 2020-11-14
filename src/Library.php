e19f421bfa9f90dbe61ea10c59d1a91969a56682
dmatthams <dmatthams@users.noreply.github.com>
2020-11-14 17:31:56 +0000

    I heard you like queries in your queries ？

--- src/Library.php
+++ src/Library.php
@@ -124,7 +124,7 @@ class Library extends Plugin
                     'template' => '_library/pages/listing',
                     'defaults' => [
                         'elementType' => 'all',
-                        'elementSource' => '',
+                        'elementSource' => 0,
                     ]
                 ];
             }
