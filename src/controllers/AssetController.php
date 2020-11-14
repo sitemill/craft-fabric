7a7afae220d7bf276687b4ef677dad261b861cf7
dmatthams <dmatthams@users.noreply.github.com>
2020-11-14 15:57:27 +0000

    Improving listing > card > thumbnail waterfall

---
+++ src/controllers/AssetController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @link      https://sitemill.co
+ * @copyright Copyright (c) 2020 SiteMill
+ */
+
+namespace sitemill\library\controllers;
+
+use craft\elements\Asset;
+use sitemill\library\Library;
+
+use Craft;
+use craft\web\Controller;
+use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+
+
+/**
+ * Class AssetController
+ *
+ * @package sitemill\library\controllers
+ */
+class AssetController
+{
+
+    /**
+     * Returns an asset’s thumbnail.
+     *
+     * @param string $uid The asset's UID
+     * @param int $width The thumbnail width
+     * @param int $height The thumbnail height
+     * @return Response
+     * @since 3.0.13
+     */
+    public function actionThumb(string $uid, int $width, int $height): Response
+    {
+        $asset = Asset::find()->uid($uid)->one();
+
+
+        if (!$asset) {
+            $e = new NotFoundHttpException('Invalid asset UID: ' . $uid);
+            Craft::$app->getErrorHandler()->logException($e);
+            return $this->asBrokenImage($e);
+        }
+
+        try {
+            $path = Craft::$app->getAssets()->getThumbPath($asset, $width, $height, true);
+        } catch (\Throwable $e) {
+            Craft::$app->getErrorHandler()->logException($e);
+            return $this->asBrokenImage($e);
+        }
+
+        return $this->response
+            ->setCacheHeaders()
+            ->sendFile($path, $asset->getFilename(), [
+                'inline' => true,
+            ]);
+    }
+
+    /**
+     * Sends a broken image response based on a given exception.
+     *
+     * @param \Throwable|null $e The exception that was thrown
+     * @return Response
+     * @since 3.4.8
+     */
+    protected function asBrokenImage(\Throwable $e = null): Response
+    {
+        $statusCode = $e instanceof HttpException && $e->statusCode ? $e->statusCode : 500;
+        return $this->response
+            ->sendFile(Craft::getAlias('@appicons/broken-image.svg'), 'nope.svg', [
+                'mimeType' => 'image/svg+xml',
+                'inline' => true,
+            ])
+            ->setStatusCode($statusCode);
+    }
+}
